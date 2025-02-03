e56f68b9b070cebe3dbd6a3350c38831ed9ed59e
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-02-03 12:22:49 +0800

    feat: create login activity

--- app/src/main/java/com/valdez/johnjoaquin/MainActivity.kt
+++ app/src/main/java/com/valdez/johnjoaquin/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.valdez.johnjoaquin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 
@@ -9,5 +11,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
     }
 }
