ee998b63d3da81c1860b6594750f151acaac61b0
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-02-10 10:41:05 +0800

    feat: create dashboard activity

--- app/src/main/java/com/valdez/johnjoaquin/LoginActivity.kt
+++ app/src/main/java/com/valdez/johnjoaquin/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.valdez.johnjoaquin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +11,10 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-
+        val signUpButton = findViewById<Button>(R.id.signUpButton)
+        signUpButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
