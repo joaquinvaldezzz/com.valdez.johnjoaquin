22a9c82511f759fa49d42dc45ea8b8fd71713aca
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-03-31 11:35:38 +0800

    feat: create login function

--- app/src/main/java/com/valdez/johnjoaquin/LoginActivity.kt
+++ app/src/main/java/com/valdez/johnjoaquin/LoginActivity.kt
@@ -1,20 +1,47 @@
 package com.valdez.johnjoaquin
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var editTextUsername: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        val signUpButton = findViewById<Button>(R.id.signUpButton)
-        signUpButton.setOnClickListener {
-            val intent = Intent(this, DashboardActivity::class.java)
-            startActivity(intent)
+
+        sharedPreferences = getSharedPreferences("userPreferences", MODE_PRIVATE)
+
+        editTextUsername = findViewById(R.id.edittext_email_address)
+        editTextPassword = findViewById(R.id.edittext_password)
+        buttonLogin = findViewById(R.id.button_login)
+
+        buttonLogin.setOnClickListener {
+            val username = editTextUsername.text.toString()
+            val password = editTextPassword.text.toString()
+            val storedUsername = sharedPreferences.getString("username", null)
+            val storedPassword = sharedPreferences.getString("password", null)
+
+            if (username == storedUsername && password == storedPassword) {
+                Toast.makeText(this, "Logged in successfully", Toast.LENGTH_LONG).show()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+
+                finish()
+            } else {
+                Toast.makeText(this, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
