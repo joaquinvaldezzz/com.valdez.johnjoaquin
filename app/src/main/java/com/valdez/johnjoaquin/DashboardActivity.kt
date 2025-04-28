d55f8366401452251894014409de07d8a775889f
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2025-04-28 21:16:17 +0800

    feat: add logout button and functionality
    This commit introduces a logout button to the dashboard activity.
    - Adds a "Logout" button to the top right corner of `activity_dashboard.xml`.
    - Implements a logout feature with a confirmation dialog in `DashboardActivity.kt`.
    - Navigates to the `LoginActivity` after confirming the logout.
    - Closes the dashboard activity after the user logout.

--- app/src/main/java/com/valdez/johnjoaquin/DashboardActivity.kt
+++ app/src/main/java/com/valdez/johnjoaquin/DashboardActivity.kt
@@ -3,8 +3,10 @@ package com.valdez.johnjoaquin
 import Email
 import EmailAdapter
 import FragmentAdapter
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,8 +21,13 @@ class DashboardActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_dashboard)
 
+        val btnLogout = findViewById<Button>(R.id.btn_logout)
         val tabLayout = findViewById<TabLayout>(R.id.dashboard_tabs)
         val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+
+        btnLogout.setOnClickListener {
+            showDialog()
+        }
 
         val fragmentAdapter = FragmentAdapter(this)
         viewPager.adapter = fragmentAdapter
@@ -59,5 +66,19 @@ class DashboardActivity : AppCompatActivity() {
             val intent = Intent(this, NewEmailActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun showDialog() {
+        val alertDialog = AlertDialog.Builder(this@DashboardActivity)
+        alertDialog.setTitle("Are you sure?")
+            .setMessage("Do you want to change the content of the TextView?")
+            .setNegativeButton("No") { dialogInterface, _ ->
+                dialogInterface.cancel()
+            }.setPositiveButton("Yes") { _, _ ->
+                val loginActivity = Intent(this, LoginActivity::class.java)
+                startActivity(loginActivity)
+                finish()
+            }.setCancelable(false)
+        alertDialog.create().show()
     }
 }
